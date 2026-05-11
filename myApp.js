530d553300c909e8a471632dbe5ab6645833ace1
JDL30 <1412664@learn.vsb.bc.ca>
2026-05-11 20:35:20 +0000

    tryed

--- myApp.js
+++ myApp.js
@@ -2,15 +2,26 @@ let express = require('express');
 let app = express();
 console.log("Hello World");
 
-app.use("/public",express.static(__dirname+"/public"));
 
-app.get("/",function(req, res) {
-  res.sendFile(__dirname + '/views/index.html')
-})
+//app.use("/public",express.static(__dirname+"/public"));
 
-app.get("/json",function(req, res) {
-  res.json({"message": "Hello json"});
-})
+//app.get("/",function(req, res) {
+//  res.sendFile(__dirname + '/views/index.html')
+//})
+require('dotenv').config()
+
+app.get("/json", function(req, res) {
+    var response = "Hello json"
+    if (process.env.MESSAGE_STYLE === "upppercase"){
+        response = "Hello json".toUpperCase();
+    }else{
+        response = "Hello json";
+    }
+    res.json({
+        "message": response
+    });
+});
+
 
 
 
