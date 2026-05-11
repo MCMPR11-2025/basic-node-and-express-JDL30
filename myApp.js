6a6face905bdde4cca1dce0a9a94992497b0c0a0
JDL30 <1412664@learn.vsb.bc.ca>
2026-05-11 20:01:45 +0000

    steps 1-5

--- myApp.js
+++ myApp.js
@@ -1,7 +1,16 @@
 let express = require('express');
 let app = express();
-
 console.log("Hello World");
+
+app.use("/public",express.static(__dirname+"/public"));
+
+app.get("/",function(req, res) {
+  res.sendFile(__dirname + '/views/index.html')
+})
+
+app.get("/json",function(req, res) {
+  res.json({"message": "Hello json"});
+})
 
 
 
